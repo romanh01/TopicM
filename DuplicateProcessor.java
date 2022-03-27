7de892f20c1d0b0dba988a7f0b141aec9abac258
Roman <94122457+romanh01@users.noreply.github.com>
2022-03-27 23:39:43 +0100

    Main class

---
+++ DuplicateProcessor.java
@@ -0,0 +1,73 @@
+package Topic_M;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class DuplicateProcessor 
+{
+	/* USING A HASHMAP TO COUNT DUPLICATES AS KEYS FOR EVERY WORD AS VALUES */
+	public static ArrayList<String> CountDuplicates(ArrayList<String> fileList, int topN)
+	{
+		LinkedHashMap<String, Integer> DuplicateMap = new LinkedHashMap<>();
+
+		  for (String s: fileList) 
+		  {
+			  Integer word_count = DuplicateMap.get(s);
+			  	
+			  if(word_count == null)
+			  {
+				  word_count = 0;
+			  }
+			  DuplicateMap.put(s, word_count +1);
+		  }
+		  System.out.println(DuplicateMap);
+		  System.out.println("-----");
+		  
+		  //LinkedHashMap preserve the ordering of elements in which they are inserted
+		  LinkedHashMap<String, Integer> SortedDuplicateMap = new LinkedHashMap<>();
+		   
+		  //Use Comparator.reverseOrder() for reverse ordering
+		  DuplicateMap.entrySet()
+		    .stream()
+		    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())) 
+		    .forEachOrdered(i -> SortedDuplicateMap.put(i.getKey(), i.getValue()));
+		   
+		  System.out.println("Sorted Duplicates" + SortedDuplicateMap);
+		  System.out.println("-----");
+		  
+		  /* CONVERT LINKEDHASHMAP TO ARRAYLIST (TEMPORARY) - DuplicateKeys - INSERTION ORDER IS KEPT */
+		  ArrayList<String> DuplicateKeys =new ArrayList<>(SortedDuplicateMap.keySet());
+		  System.out.println("Whats inside this arraylist that was converted from a linkedhashmap: " + DuplicateKeys);
+		  System.out.println("-----");
+		    
+		  // Clear fileList Elements to then populate it with sorted by duplicate count elements
+		  fileList.clear();
+		  
+		  
+		  /* FOR LOOP that iterates N amount of times (DuplicateKeys) and stores first N elements in fileList */
+	       for(int i = 0; i < topN; i++)
+	       {  
+	    	   fileList.add(DuplicateKeys.get(i));  
+	       }
+		  
+		return fileList;
+	}
+	
+	@SuppressWarnings("unchecked")
+	/* CALCULATES ARRAYLIST SIMILARITY %*/
+	public static void CompareLists(ArrayList<String> fileList1, ArrayList<String> fileList2)
+	{
+		float verdict;
+		ArrayList<String> retainList = new ArrayList<String>();
+		retainList = (ArrayList<String>)fileList1.clone();
+		
+		retainList.retainAll(fileList2);
+		System.out.println("After retainAll: " + retainList);
+		
+		float retainListsize = retainList.size();
+		verdict = ((retainListsize/10)*100);
+		System.out.println("Similarity:" +verdict +"%");
+	}
+}
